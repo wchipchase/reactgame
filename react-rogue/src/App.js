aeb4d3ef5f106dc031606a08d1689074f95909f2
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-12-15 19:13:21 -0600

    player movement working

---
+++ react-rogue/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactRogue from './ReactRogue'
+
+const App = () => 
+    <div className="App">
+      <ReactRogue width={40} height={40} tilesize={16} />
+    </div>
+;
+
+
+export default App;
---
+++ react-rogue/src/ReactRogue.js
@@ -0,0 +1,44 @@
+import React, {useRef, useEffect, useState} from 'react';
+import InputManager from './inputManager';
+
+const ReactRogue = ({width, height, tilesize}) => {
+  const canvasRef = React.useRef();
+  const [player, setPlayer] = useState({x: 64, y: 128})
+  let inputManager = new InputManager();
+
+  const handleInput = (action, data) => {
+    console.log(`handle input: ${action}:${JSON.stringify(data)}`)
+    let newPlayer = {...player };
+    newPlayer.x += data.x * tilesize;
+    newPlayer.y += data.y * tilesize;
+    setPlayer(newPlayer);
+  }
+
+
+
+  useEffect(() =>{
+    console.log('Bind input');
+    inputManager.bindKeys();
+    inputManager.subscribe(handleInput);
+    return() => {
+      inputManager.unbindKeys();
+      inputManager.unsubscribe(handleInput);
+    }
+  })
+
+  useEffect(() => {
+    console.log('Draw to canvas');
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.clearRect(0, 0, width * tilesize, height * tilesize);
+    ctx.fillStyle = '#000';
+    ctx.fillRect(player.x, player.y, 16, 16);
+  })
+  return (
+    <canvas 
+      ref ={canvasRef}
+      width={width * tilesize} 
+      height={height * tilesize} 
+      style={{border: '1px solid black'}}>
+  </canvas>)};
+
+export default ReactRogue;
