e1781cc9224236c53a584390811abb0b63866acf
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-12-17 14:05:20 -0600

    stairs not working

--- react-rogue/src/ReactRogue.js
+++ react-rogue/src/ReactRogue.js
@@ -1,18 +1,22 @@
 import React, {useRef, useEffect, useState} from 'react';
 import InputManager from './inputManager';
+import Player from './Player';
+import World from './World';
+import Spawner from './Spawner';
 
 const ReactRogue = ({width, height, tilesize}) => {
   const canvasRef = React.useRef();
-  const [player, setPlayer] = useState({x: 64, y: 128})
+  // const [player, setPlayer] = useState(new Player(1, 2, tilesize))
+  const [world, setWorld] = useState(new World(width, height, tilesize));
   let inputManager = new InputManager();
 
   const handleInput = (action, data) => {
     console.log(`handle input: ${action}:${JSON.stringify(data)}`)
-    let newPlayer = {...player };
-    newPlayer.x += data.x * tilesize;
-    newPlayer.y += data.y * tilesize;
-    setPlayer(newPlayer);
-  }
+    let newWorld = new World();
+    Object.assign(newWorld, world);
+    newWorld.movePlayer(data.x, data.y);
+    setWorld(newWorld);
+  };
 
 
 
@@ -30,15 +34,41 @@ const ReactRogue = ({width, height, tilesize}) => {
     console.log('Draw to canvas');
     const ctx = canvasRef.current.getContext('2d');
     ctx.clearRect(0, 0, width * tilesize, height * tilesize);
-    ctx.fillStyle = '#000';
-    ctx.fillRect(player.x, player.y, 16, 16);
+    world.draw(ctx);
   })
+
+  useEffect(() => {
+    console.log('Create Map!');
+    let newWorld = new World();
+    Object.assign(newWorld, world);
+    newWorld.createCellularMap();
+    newWorld.moveToSpace(world.player);
+    let spawner = new Spawner(newWorld);
+    spawner.spawnLoot(10);
+    spawner.spawnMonsters(6);
+    setWorld(newWorld);
+  }, [])
+
   return (
-    <canvas 
-      ref ={canvasRef}
-      width={width * tilesize} 
-      height={height * tilesize} 
-      style={{border: '1px solid black'}}>
-  </canvas>)};
+    <>
+      <canvas 
+        ref ={canvasRef}
+        width={width * tilesize} 
+        height={height * tilesize} 
+        style={{border: '1px solid black', background: 'DimGray'}}>
+      </canvas>
+      <ul>
+        {world.player.inventory.map((item, index) => (
+          <li key={index}>{item.attributes.name}</li>
+          ))}
+      </ul>
+
+      <ul>
+      {world.history.map((item, index) => (
+        <li key={index}>{item}</li>
+        ))}
+    </ul>
+    </>
+  )};
 
 export default ReactRogue;
